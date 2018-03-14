38d63deb1a089eebc3c353d45f9d44ac304cfefe
Konstantin Belyalov <arsenicus@mail.ru>
2018-03-14 08:35:29 -0700

    When setup done - redirect to IP address of STA interface.

--- ui/js/setup_done.js
+++ ui/js/setup_done.js
@@ -6,13 +6,16 @@
 
 function done_config_update(c)
 {
+    console.log(document.location);
     if (current_config['wifi']['connected']) {
         var c = current_config['wifi'];
         $('#done_connected').html(c['ssid']);
         $('#done_ip').html(c['ifconfig']['ip']);
+        $('#done_finish_btn').attr('href', 'http://{0}:{1}/'.format(c['ifconfig']['ip'], document.location.port))
     } else {
         $('#done_connected').html('Not Connected');
         $('#done_ip').html('N/A');
+        $('#done_finish_btn').attr('href', '/');
     }
     $('#done_http').html(current_config['http']['enabled'] ? 'Enabled' : 'Disabled');
     $('#done_mqtt').html(current_config['mqtt']['enabled'] ? 'Enabled' : 'Disabled');
