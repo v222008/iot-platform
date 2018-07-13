95a0e68e97d435cbf8dafc5039dd47c89077a876
Konstantin Belyalov <arsenicus@mail.ru>
2018-07-12 18:20:03 -0700

    Add Magic Led controller

---
+++ devices/magic_led_controller/main.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env micropython
+"""
+Home Magic mini LED Controller
+
+MIT license
+(C) Konstantin Belyalov 2017-2018
+"""
+
+import logging
+import micropython
+import machine
+import network
+import uasyncio as asyncio
+
+import tinyweb
+import tinydns
+import tinymqtt
+
+import platform.utils.captiveportal
+from platform.btn.setup import SetupButton
+from platform.utils.wifi import WifiSetup
+from platform.utils.config import SimpleConfig
+from platform.exclog import Exclog, log_exception
+
+from strip import WhiteLedStrip
+
+
+white_pin = const(13)
+blue_pin = const(12)
+red_pin = const(14)
+green_pin = const(5)
+status_led_pin = const(10)
+
+log = logging.Logger('main')
+
+
+async def shutdown_wait():
+    """Helper to make graceful app shutdown"""
+    await asyncio.sleep_ms(100)
+
+
+def main():
+    # Some ports requires to allocate extra mem for exceptions
+    if hasattr(micropython, 'alloc_emergency_exception_buf'):
+        micropython.alloc_emergency_exception_buf(100)
+
+    loop = asyncio.get_event_loop()
+    logging.basicConfig(level=logging.DEBUG)
+
+    # Base config
+    config = SimpleConfig()
+    config.add_param('configured', False)
+    wsetup = WifiSetup(config)
+
+    # MQTT
+    mqtt = tinymqtt.MQTTClient('LEDcontroller-{}'.format(
+        platform.utils.mac_last_digits()), config=config)
+
+    # DNS
+    dns = tinydns.Server(ttl=10)
+
+    # WebServer
+    web = tinyweb.webserver()
+
+    # Enable REST API for config & wifi
+    web.add_resource(config, '/config')
+    web.add_resource(wsetup, '/wifi')
+    web.add_resource(Exclog(), '/exclog')
+
+    # Create LED strip handler
+    WhiteLedStrip(machine.Pin(green_pin), config, web, mqtt, loop)
+
+    # Peripheral modules
+    setupbtn = SetupButton(config, None)
+
+    # Other web routes
+    @web.route('/')
+    async def index(req, resp):
+        if config.configured:
+            await resp.redirect('/dashboard')
+        else:
+            await resp.redirect('/setup')
+
+    @web.route('/dashboard')
+    async def page_dashboard(req, resp):
+        await resp.send_file('dashboard_all.html.gz',
+                             content_encoding='gzip',
+                             content_type='text/html')
+
+    @web.route('/setup')
+    async def page_setup(req, resp):
+        await resp.send_file('setup_all.html.gz',
+                             content_encoding='gzip',
+                             content_type='text/html')
+
+    # Setup AP parameters
+    ap_if = network.WLAN(network.AP_IF)
+    essid = b'LedCtrl-%s' % platform.utils.mac_last_digits()
+    ap_if.active(True)
+    ap_if.config(essid=essid, authmode=network.AUTH_WPA_WPA2_PSK, password=b'ledledled')
+    ap_if.ifconfig(('192.168.168.1', '255.255.255.0', '192.168.168.1', '192.168.168.1'))
+    ap_if.active(False)
+    # Captive portal
+    platform.utils.captiveportal.enable(web, dns, '192.168.168.1')
+
+    # Load configuration
+    try:
+        config.load()
+    except Exception as e:
+        log.warning('Config load failed: {}'.format(e))
+        pass
+
+    # Main loop
+    try:
+        wport = 80
+        dport = 53
+        if platform.utils.is_emulator():
+            wport = 8080
+            dport = 5335
+        # Start services
+        dns.run(host='0.0.0.0', port=dport, loop=loop)
+        web.run(host='0.0.0.0', port=wport, loop_forever=False, loop=loop)
+        mqtt.run(loop)
+        setupbtn.run(loop)
+
+        # Run main loop
+        loop.run_forever()
+    except KeyboardInterrupt as e:
+        if platform.utils.is_emulator():
+            print('terminating...')
+            for s in [web, dns, mqtt]:
+                s.shutdown()
+            loop.run_until_complete(shutdown_wait())
+            print('Done')
+        else:
+            raise
+    except Exception as e:
+        log_exception(e)
+
+
+if __name__ == '__main__':
+    main()
---
+++ devices/magic_led_controller/strip.py
@@ -0,0 +1,135 @@
+"""
+Magic LED Controller
+
+MIT license
+(C) Konstantin Belyalov 2017-2018
+"""
+import logging
+import machine
+import ujson as json
+import uasyncio as asyncio
+from platform.exclog import log_exception
+
+log = logging.getLogger('LEDSTRIP')
+
+
+class StripError(Exception):
+    pass
+
+
+class NoSuchEffectError(Exception):
+    pass
+
+
+class WhiteLedStrip():
+    def __init__(self, pin, config, web, mqtt, loop):
+        self.cfg = config
+        self.effects = ['on', 'off', 'fade']
+        # Params
+        self.cfg.add_param('led_last_brightness', '100')
+        # MQTT
+        self.mqtt = mqtt
+        self.cfg.add_param('mqtt_topic_led_status', 'lights')
+        # Control topic. We need to get notified to re-subscribe on it
+        self.cfg.add_param('mqtt_topic_led_control', 'lights/set',
+                           callback=self._mqtt_config_changed,
+                           group='mqtt_config')
+        self.pwm = machine.PWM(pin, freq=1000)
+        # Animation - e.g. fade effect
+        self.anim = None
+        # Web endpoints
+        for act in self.effects:
+            web.add_resource(self, '/{}'.format(act), action=act)
+        self.loop = loop
+
+    def _extract_brightness(self, data):
+        brightness = self.cfg.led_last_brightness
+        if 'brightness' in data:
+            brightness = int(data['brightness'])
+            if brightness > 100 or brightness < 0:
+                raise ValueError('Invalid value {}, must be in range [0-100].')
+        return brightness
+
+    def _mqtt_config_changed(self):
+        """Callback when mqtt control topic changed"""
+        self.mqtt.subscribe(self.cfg.mqtt_topic_led_control, self.mqtt_control)
+
+    def _publish_mqtt_state(self, state):
+        print("published", self.cfg.mqtt_topic_led_status, state)
+        self.mqtt.publish(self.cfg.mqtt_topic_led_status, str(state), retain=True)
+
+    def on(self, data):
+        val = self._extract_brightness(data)
+        self.pwm.duty(val * 1024 // 100)
+        self._publish_mqtt_state(val)
+        self.cfg.led_last_brightness = val
+
+    def off(self, data):
+        self.pwm.duty(0)
+        self._publish_mqtt_state(0)
+
+    async def _fade_effect(self, new_brightness, length, delay):
+        # Run effect
+        val = self.cfg.led_last_brightness
+        print("new cur", new_brightness, val)
+        inc = (new_brightness - val) // length
+        for step in range(length):
+            val += inc
+            self.pwm.duty(val * 1024 // 100)
+            await asyncio.sleep_ms(delay)
+        # Finally after effect finished - publish mqtt status update
+        self._publish_mqtt_state(new_brightness)
+        self.cfg.led_last_brightness = new_brightness
+
+    def fade(self, data):
+        val = self._extract_brightness(data)
+        length = data.get('length', 20)
+        delay = data.get('delay', 20)
+        if self.anim:
+            asyncio.cancel(self.anim)
+        self.loop.create_task(self._fade_effect(val,
+                                                length, delay))
+
+    def process_command(self, data, action):
+        # by default - all pixels
+        if not hasattr(self, action):
+            raise NoSuchEffectError('No such effect {}'.format(action))
+        act = getattr(self, action)
+        act(data)
+
+    def post(self, data, action):
+        try:
+            self.process_command(data, action)
+            return {'message': 'success'}
+        except NoSuchEffectError as e:
+            return {'message': e}, 404
+        except ValueError as e:
+            return {'message': e}, 400
+        except Exception as e:
+            log_exception(e)
+
+    def get(self, data, action):
+        return self.post(data, action)
+
+    def mqtt_control(self, data):
+        # First case is pretty simple - brightness as value
+        try:
+            val = int(data)
+            self.process_command({'brightness': val}, 'fade')
+            return
+        except ValueError:
+            pass
+        except Exception as e:
+            log_exception(e)
+            return
+
+        # One more chance - try to use it as JSON
+        try:
+            js = json.loads(data)
+            act = js.get('effect', 'fade')
+            self.process_command(js, act)
+            return
+        except ValueError:
+            pass
+        except Exception as e:
+            log_exception(e)
